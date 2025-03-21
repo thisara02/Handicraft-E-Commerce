94964f3ebd7ea9d09b5cd4bda5978be0ac428a2b
Thisara Madusanka <137640738+thisara02@users.noreply.github.com>
2025-03-21 23:50:43 +0530

    done migrations

---
+++ Server/database/migrations/2025_03_21_021140_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('venID')->constrained('vendors')->onDelete('cascade');
+            $table->text('description');
+            $table->string('prodName');
+            $table->decimal('price', 10, 2);
+            $table->string('category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ Server/database/migrations/2025_03_21_021654_create_reviews_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cusID')->constrained('customers')->onDelete('cascade');
+            $table->foreignId('prodID')->constrained('products')->onDelete('cascade');
+            $table->integer('starRate');
+            $table->text('feedback');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
